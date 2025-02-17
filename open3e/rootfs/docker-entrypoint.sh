9510a294f2dd82fdcea9d4f22cd6a247d6a5801a
flecke <57179324+flecke-m@users.noreply.github.com>
2025-02-17 09:37:50 +0100

    Create docker-entrypoint.sh

---
+++ open3e/rootfs/docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bashio
+
+bashio::log.info "Preparing to start...checking for devices.json"
+
+if ! test -f /open3e/devices.json; then
+   bashio::log.info "Running open3e depcit system ... This may take a while"
+   cd /open3e
+   CMD open3e_depictSystem -c can0 
+fi
+
+bashio::log.info "Starting Open3e..."
+cd /open3e
+CMD open3e @args
