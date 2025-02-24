114b5ab149202cebe4b051a5e7b62bbb1aed6c67
flecke-m <matthiasfleckenstein@gmail.com>
2025-02-24 21:35:02 +0100

    modified:   config.yaml 	modified:   rootfs/run.sh

--- rootfs/run.sh
+++ rootfs/run.sh
@@ -6,13 +6,13 @@ bashio::log.info "Preparing to start...reading config"
 
 CAN=$(bashio::config 'main.can')
 bashio::log.info "Can Interface:  $CAN"
-ListenTopic=$(bashio::config 'main.Listen-Topic')
-bashio::log.info "Listen to topic:  $ListenTopic"
+LISTENTOPIC=$(bashio::config 'main.Listen_Topic')
+bashio::log.info "Listen to topic:  $LISTENTOPIC"
 SERVER=$(bashio::config 'mqtt.Server_Topic')
 bashio::log.info "MQTT Server and Publish Topic:  $SERVER"
-USERPWD=$(bashio::config 'mqtt.MQTT-User-Password')
+USERPWD=$(bashio::config 'mqtt.MQTT_UserPassword')
 bashio::log.info "MQTT Username/Password:  $USERPWD"
-FORMATSTRING=$(bashio::config 'mqtt.MQTT-Format-String')
+FORMATSTRING=$(bashio::config 'mqtt.MQTT_FormatString')
 bashio::log.info "Format String:  $FORMATSTRING"
 CLIENTID=$(bashio::config 'mqtt.MQTT_ClientID')
 bashio::log.info "ClientID:  $CLIENTID"    
@@ -33,4 +33,4 @@ fi
 
 bashio::log.info "Starting Open3e..."
 cd /data
-open3e --can $CAN --mqtt $SERVER --mqttuser $USERPWD --mqttformatstring $FORMATSTRING --mqttclientid $CLIENTID --listen $ListenTopic --config /data/devices.json
+open3e --can $CAN --mqtt $SERVER --mqttuser $USERPWD --mqttformatstring $FORMATSTRING --mqttclientid $CLIENTID --listen $LISTENTOPIC --config /data/devices.json
